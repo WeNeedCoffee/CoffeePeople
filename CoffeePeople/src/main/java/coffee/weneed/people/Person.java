464ffa79885751cd50787c3935b50805ca6ebb75
Daleth Darko <Daleth@Dalethium.com>
2019-05-09 09:20:42 -0700

    Initial Commit

---
+++ CoffeePeople/src/main/java/coffee/weneed/people/Person.java
@@ -0,0 +1,21 @@
+package coffee.weneed.people;
+
+import org.json.JSONObject;
+import coffee.weneed.utils.dataholders.IJSONObjectDataHolder;
+
+public class Person implements IJSONObjectDataHolder {
+
+	@Override
+	public void fromJSON(JSONObject json) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public JSONObject toJSON() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	
+}
